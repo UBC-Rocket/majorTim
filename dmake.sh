1b331e3a0d4eec6001431bdb0d66485e25852368
esemeniuc <3838856+esemeniuc@users.noreply.github.com>
2018-01-21 16:15:15 -0800

    working dmake from within board specific folder

--- dmake.sh
+++ dmake.sh
@@ -6,4 +6,4 @@
 
 USER_ID=`id -u`
 GROUP_ID=`id -g`
-docker run -it --volume `pwd`:/home/rkt --user=$USER_ID:$GROUP_ID ubcrocket/majortim-build /bin/bash -c "sed 's:BINPATH = :BINPATH=/home/rkt/gcc-arm-none-eabi-7-2017-q4-major/bin:' Makefile > Makefile_with_path; make -f Makefile_with_path $1"
+docker run -it --volume `pwd`:/home/rkt/build --user=$USER_ID:$GROUP_ID ubcrocket/majortim-build /bin/bash -c "sed 's:BINPATH = :BINPATH=/home/rkt/gcc-arm-none-eabi-7-2017-q4-major/bin:' Makefile > Makefile_with_path; make -f Makefile_with_path $1"
