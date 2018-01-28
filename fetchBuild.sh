715e8b315b5a2c0df5adb5dcac2b6984b9b2803a
Rai McKenzie <rai.mckenziee@gmail.com>
2018-01-28 12:33:08 -0800

    Cleaned script up.

--- fetchBuild.sh
+++ fetchBuild.sh
@@ -4,9 +4,7 @@ if [ -z "$var" ]; then
 	echo "Please specify a circleCI build number"
 	echo "Builds are listed here: https://circleci.com/gh/UBC-Rocket/majorTim"
 else
-	mkdir builds
-	cd builds
-	mkdir build-$1
-	cd build-$1
+	mkdir -p builds/build-$1
+	cd builds/build-$1
 	wget https://$1-118362338-gh.circle-artifacts.com/0/tmp/apdet_hanna.bin
 fi
