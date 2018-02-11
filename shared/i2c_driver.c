fe06fbde9aa9c7485aed885704455c473b943f63
Matt Peterson <mattpeterson145@gmail.com>
2018-02-10 17:36:41 -0800

    WIP: trying to get I2C driver to work with ARM mbed.

--- shared/i2c_driver.c
+++ shared/i2c_driver.c
@@ -1,92 +1,23 @@
-/* I2C Driver */
+/* I2C Driver Source*/
 
 #include <math.h>
 #include <stdint.h>
 
-#include "../apdet_hanna/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_i2c.h"
-#include "./general.h"
+#include "I2C.h"
+#include "i2c_driver.h"
+#include "general.h"
 
-/* Constants -------------------------------------------------- */
-#define I2C_SCL_CLOCK       100000
-#define I2C_READ_TIMEOUT    100
-#define I2C_WRITE_TIMEOUT   100
-
-#define BAROMETER_ADDRESS       0x76
-#define ACCELEROMETER_ADDRESS   0x18
-
-#define BAROMETER_CMD_RESET     0x1E
-#define BAROMETER_CMD_PROM_READ 0xA0
-#define BAROMETER_CMD_ADC_READ  0x00
-#define BAROMETER_CMD_ADC_CONV  0x40
-#define BAROMETER_CMD_ADC_D1    0x00
-#define BAROMETER_CMD_ADC_D2    0x10
-#define BAROMETER_CMD_ADC_256   0x00
-#define BAROMETER_CMD_ADC_512   0x02
-#define BAROMETER_CMD_ADC_1024  0x04
-#define BAROMETER_CMD_ADC_2048  0x06
-#define BAROMETER_CMD_ADC_4096  0x08
-
-#define ACCELEROMETER_AUTO_INCREMENT        0x80
-#define ACCELEROMETER_REG_CTRL_REG1         0x20
-#define ACCELEROMETER_REG_CTRL_REG2         0x21
-#define ACCELEROMETER_REG_CTRL_REG3         0x22
-#define ACCELEROMETER_REG_CTRL_REG4         0x23
-#define ACCELEROMETER_REG_CTRL_REG5         0x24
-#define ACCELEROMETER_REG_HP_FILTER_RESET   0x25
-#define ACCELEROMETER_REG_REFERENCE         0x26
-#define ACCELEROMETER_REG_STATUS_REG        0x27
-#define ACCELEROMETER_REG_OUT_X_L           0x28
-#define ACCELEROMETER_REG_OUT_X_H           0x29
-#define ACCELEROMETER_REG_OUT_Y_L           0x2A
-#define ACCELEROMETER_REG_OUT_Y_H           0x2B
-#define ACCELEROMETER_REG_OUT_Z_L           0x2C
-#define ACCELEROMETER_REG_OUT_Z_H           0x2D
-#define ACCELEROMETER_REG_INT1_CFG          0x30
-#define ACCELEROMETER_REG_INT1_SOURCE       0x31
-#define ACCELEROMETER_REG_INT1_THS          0x32
-#define ACCELEROMETER_REG_INT1_DURATION     0x33
-#define ACCELEROMETER_REG_INT2_CFG          0x34
-#define ACCELEROMETER_REG_INT2_SOURCE       0x35
-#define ACCELEROMETER_REG_INT2_THS          0x36
-#define ACCELEROMETER_REG_INT2_DURATION     0x37
+/* Objects -------------------------------------------------- */
+I2C i2c(SDA, SCL); //TODO: pins
 
 /* Variables -------------------------------------------------- */
-static I2C_HandleTypeDef hi2c;
 static uint16_t barometer_calibration[6];
 
 /* I2C Functions -------------------------------------------------- */
 
 /**
-  * @brief  Initializes the I2C controller
-  * @retval Status
-  */
-extern status_t i2cInit(void)
-{
-    if (HAL_I2C_DeInit(&hi2c) != HAL_OK) {
-        return STATUS_ERROR;
-    }
-
-    hi2c.Instance = I2C1;
-    hi2c.Init.ClockSpeed = I2C_SCL_CLOCK;
-    hi2c.Init.DutyCycle = I2C_DUTYCYCLE_2;
-    hi2c.Init.OwnAddress1 = 0;
-    hi2c.Init.AddressingMode = I2C_ADDRESSINGMODE_7BIT;
-    hi2c.Init.DualAddressMode = I2C_DUALADDRESS_DISABLE;
-    hi2c.Init.OwnAddress2 = 0;
-    hi2c.Init.GeneralCallMode = I2C_GENERALCALL_DISABLE;
-    hi2c.Init.NoStretchMode = I2C_NOSTRETCH_DISABLE;
-
-    if (HAL_I2C_Init(&hi2c) != HAL_OK) {
-        return STATUS_ERROR;
-    }
-
-    // TODO init MSP
-
-    return STATUS_OK;
-}
-
-/**
   * @brief  Reads data from the I2C bus
+  * @note   This is a single master system so repeated start conditions have been disabled
   * @param  address The address of the device to read from
   * @param  data A pointer to the buffer to write the data to
   * @param  size The number of bytes to read
@@ -94,7 +25,7 @@ extern status_t i2cInit(void)
   */
 extern status_t i2cRead(uint16_t address, uint8_t *data, uint16_t size)
 {
-    if (HAL_I2C_Master_Receive(&hi2c, address, data, size, I2C_READ_TIMEOUT) != HAL_OK) {
+    if (i2c.read((int)address, (char*)data, (int)size, FALSE) != 0) {
         return STATUS_ERROR;
     }
 
@@ -103,6 +34,7 @@ extern status_t i2cRead(uint16_t address, uint8_t *data, uint16_t size)
 
 /**
   * @brief  Writes data to the I2C bus
+  * @note   This is a single master system so repeated start conditions have been disabled
   * @param  address The address of the device to write to
   * @param  data A pointer to the buffer to read the data from
   * @param  size The number of bytes to write
@@ -110,7 +42,7 @@ extern status_t i2cRead(uint16_t address, uint8_t *data, uint16_t size)
   */
 extern status_t i2cWrite(uint16_t address, uint8_t *data, uint16_t size)
 {
-    if (HAL_I2C_Master_Transmit(&hi2c, address, data, size, I2C_WRITE_TIMEOUT) != HAL_OK) {
+    if (i2c.write((int)address, (char*)data, (int)size, FALSE) != 0) {
         return STATUS_ERROR;
     }
 
@@ -194,7 +126,7 @@ extern status_t barometerGetData(uint8_t d, uint8_t osr, uint8_t *buffer)
     /* Delay as needed to allow ADC conversion to complete */
     switch (osr & 0x0f) {
         case BAROMETER_CMD_ADC_256:
-            HAL_Delay(1);
+            HAL_Delay(1); //TODO: fix implicit declaration
             break;
         case BAROMETER_CMD_ADC_512:
             HAL_Delay(2);
@@ -392,7 +324,7 @@ extern status_t accelerometerWriteRegister(uint8_t sub_address, uint8_t data)
   */
 extern status_t accelerometerInit(void)
 {
-    return STATUS_OK;
+    return STATUS_OK; //TODO: configure registers if needed
 }
 
 /**
@@ -404,15 +336,15 @@ extern status_t accelerometerInit(void)
   */
 extern status_t accelerometerGetData(int16_t *x, int16_t *y, int16_t *z)
 {
-    uint16_t buffer[6];
+    uint8_t buffer[6];
 
     if (accelerometerReadRegister(ACCELEROMETER_REG_OUT_X_L, buffer, 6) != STATUS_OK) {
         return STATUS_ERROR;
     }
 
-    *x = buffer[0] | (buffer[1] << 8);
-    *y = buffer[2] | (buffer[3] << 8);
-    *z = buffer[4] | (buffer[5] << 8);
+    *x = (uint16_t)buffer[0] | ((uint16_t)buffer[1] << 8);
+    *y = (uint16_t)buffer[2] | ((uint16_t)buffer[3] << 8);
+    *z = (uint16_t)buffer[4] | ((uint16_t)buffer[5] << 8);
 
     return STATUS_OK;
 }
