9d1d05cdf66e55ad3dcbfeef488b6fb4327bfa37
jeremy <jeremy.quicklearner@gmail.com>
2018-02-11 11:55:59 -0800

    Newly built binaries are now moved to builds directory

--- build.sh
+++ build.sh
@@ -55,6 +55,7 @@ if [ ! $1 ] || [ $1 = build ]; then
 		echo -e '\033[0;32m'
 		figlet "Build Successful!"
 		echo -e '\033[0m'
+		cp $BUILD_DIR/mbed.bin "../builds/"$BOARD"_"$(date +"%Y%m%d_%H%M%S").bin
 	fi
 
 # Flash the image to a board
