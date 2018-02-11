90e2502a89de08a25989972ea149ca46ce475762
Rai McKenzie <rai.mckenziee@gmail.com>
2018-02-11 14:19:33 -0800

    removed timestamp from build

--- build.sh
+++ build.sh
@@ -56,7 +56,7 @@ if [ ! $1 ] || [ $1 = build ]; then
 		echo -e '\033[0;32m'
 		figlet "Build Successful!"
 		echo -e '\033[0m'
-		cp $BUILD_DIR/mbed.bin "../builds/"$BOARD"_"$(date +"%Y%m%d_%H%M%S").bin
+		cp $BUILD_DIR/mbed.bin "../builds/"$BOARD.bin
 	fi
 
 # Flash the image to a board
