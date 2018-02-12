7003d29933ddedac4a42ace94c4f3d814d876f86
iandelrio <iandelriow@gmail.com>
2018-02-11 16:13:40 -0800

    Added states to general.h

--- shared/include/general.h
+++ shared/include/general.h
@@ -7,4 +7,16 @@ typedef enum status_enum {
 	STATUS_ERROR     = -1,
 } status_t;
 
+typedef enum state_enum {
+	APDET_STATE_TESTING			= 0,
+	APDET_STATE_STANDBY			= 1,
+	APDET_STATE_POWERED_ASCENT	= 2,
+	APDET_STATE_COASTING 		= 3,
+	APDET_STATE_APOGEE_TESTING	= 4,
+	APDET_STATE_DEPLOY_DROGUE	= 5,
+	APDET_STATE_INITIAL_DESCENT	= 6,
+	APDET_STATE_DEPLOY_MAIN		= 7,
+	APDET_STATE_LANDED			= 8,
+} state_t;
+
 #endif
